079667e7585c7fa656104854e4a06370732f0a43
bcaneba <bcaneba@gmail.com>
2018-07-11 06:31:38 -0500

    adding in comment form

---
+++ app/javascript/item/components/CommentForm.jsx
@@ -0,0 +1,74 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import queryString from 'query-string';
+// import axios from 'axios';
+
+// // import ItemsDisplay from './ItemsDisplay';
+
+// // const cardStyle = {
+// //   width: '18rem'
+// // };
+
+// // const itemDisplayImageSize = {
+// //   height: '18rem'
+// // };
+
+// // const cardStyle = {
+// //   height: '100%'
+// // };
+
+
+// // import itemDisplayImage from '../../../assets/images/item-placeholder.png';
+
+
+// class CommentForm extends React.Component {
+//   constructor (props) {
+//     super(props);
+//     this.state = {
+//       item: this.props.item
+//     };
+//   }
+
+//   // componentWillMount () {
+//   //   this.fetchItem(1);
+//   // }
+
+//   componentWillMount () {
+//     // this.fetchItem(1);
+//     // this.fetchItem();
+//     console.log(this.props);
+//   }
+
+//   // 
+//   // handleClick (id) {
+//   //   console.log(id)
+//   //   console.log('clicked')
+//   //   window.location.href = "http://localhost:3000/items/" + String(id)
+//   // };
+
+
+//   // changeStateExample () {
+//   //   console.log('hello there');
+//   //   this.setState({item: {id: 1, name: 'new name', price: '2000'}});
+//   //   console.log(this.state.item);
+//   // }
+
+
+
+//   render () {
+//     return (
+//       <div className='col-sm-3 pad20-top pad20-bottom'>
+//         {this.props.item}
+//       </div>
+//     );
+//   }
+// }
+
+
+// export default CommentForm;
+
+
+
+
+
+
