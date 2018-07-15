80d8f028b17ff0a94599cbc5c3f0a7bdbbd8d793
bcaneba <bcaneba@gmail.com>
2018-07-14 19:04:16 -0500

    adding js testing unit for react

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -2,4 +2,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+environment.plugins.get('Manifest').opts.writeToFileEmit = process.env.NODE_ENV !== 'test'
 module.exports = environment.toWebpackConfig()
