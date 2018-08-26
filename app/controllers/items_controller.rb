7c943a0fe01b5b70293d7294361d3123b65ea9a8
bcaneba <bcaneba@gmail.com>
2018-08-26 11:53:39 -0500

    added comments to item show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,12 @@ class ItemsController < ApplicationController
 
 	def show
 		@item = Item.find(params[:id])
+
+		respond_to do |format|
+	    format.html
+	    # format.json { render :json => @item.to_json(:include => :comments) }
+	  end
+		# binding.pry
 	end
 
 
